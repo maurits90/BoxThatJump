15c28e8ba83d19ff5df939ac9a58908c364aaa34
Maurits <mauritshudaibi@gmail.com>
2022-01-13 22:31:07 +0700

    Update playerMovement.cs

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -11,6 +11,9 @@ public class playerMovement : MonoBehaviour
     public float jumpHeight;
     bool onGround;
 
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
     float score;
 
     public Text scoretxt;
@@ -26,29 +29,60 @@ public class playerMovement : MonoBehaviour
     {
         scoretxt.text = "" + score.ToString("F0");
         score += Time.deltaTime;
+        Jump();
+        BetterJump();
+    }
+
+    void Jump()
+    {
         if (Input.GetKeyDown("space") || Input.GetKeyDown("up"))
         {
-            
-            if(onGround == true)
+
+            if (onGround == true)
             {
                 rb.velocity = Vector2.up * jumpHeight;
                 onGround = false;
                 sound.playJump();
             }
         }
-        foreach(Touch touch in Input.touches)
+        foreach (Touch touch in Input.touches)
         {
-            if(touch.phase == TouchPhase.Began)
-            { 
-                if(onGround == true)
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (onGround == true)
                 {
                     rb.velocity = Vector2.up * jumpHeight;
                     onGround = false;
+                    sound.playJump();
                 }
             }
         }
-
     }
+
+    void BetterJump()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (rb.velocity.y < 0)
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 9)* Time.deltaTime;
+            }
+            else if (rb.velocity.y > 0 && touch.phase != TouchPhase.Began)
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 2) * Time.deltaTime;
+            }
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         onGround = true;
